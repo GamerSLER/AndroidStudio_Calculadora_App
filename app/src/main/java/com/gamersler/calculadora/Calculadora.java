f94758a05eea956e3131cac048f592be482e40ea
rayden <raydenldon@gmail.com>
2025-11-21 12:01:23 +0100

    Calculadora Terminada

--- app/src/main/java/com/gamersler/calculadora/Calculadora.java
+++ app/src/main/java/com/gamersler/calculadora/Calculadora.java
@@ -1,8 +1,10 @@
 package com.gamersler.calculadora;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -11,6 +13,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.bumptech.glide.Glide;
 
 import org.mariuszgromada.math.mxparser.Expression;
 
@@ -22,6 +26,7 @@ public class Calculadora extends AppCompatActivity {
 
     TextView textoTV;
     String textoActual = "";
+    ImageView patricioIV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,17 +38,15 @@ public class Calculadora extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        patricioIV = findViewById(R.id.NaN_error);
+        patricioIV.setVisibility(patricioIV.GONE);
 
         textoTV = findViewById(R.id.TextoTV);
-        Double resultado = 0.0;
 
         Button Boton_borrar = findViewById(R.id.B_borrar);
         Button Boton_borrar_ultimo = findViewById(R.id.B_borrar_ultimo);
         Boton_borrar.setOnClickListener(v -> borrarTodo());
         Boton_borrar_ultimo.setOnClickListener(v -> borrarUltimo());
-
-
-        Button Boton_coma = findViewById(R.id.B_coma);
 
         List<Button> botonesNumericos = new ArrayList<>(Arrays.asList(
                 findViewById(R.id.B_num0),
@@ -68,11 +71,6 @@ public class Calculadora extends AppCompatActivity {
                 findViewById(R.id.B_coma)
         ));
 
-        List<Button> botonesEliminar = new ArrayList<>(Arrays.asList(
-                findViewById(R.id.B_borrar),
-                findViewById(R.id.B_borrar_ultimo)
-        ));
-
         for (Button boton_numerico: botonesNumericos) {
             boton_numerico.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -95,7 +93,6 @@ public class Calculadora extends AppCompatActivity {
         }
     }
 
-
     public void borrarUltimo() {
         if (textoActual.length() > 0) {
             textoActual = textoActual.substring(0, textoActual.length() - 1);
@@ -106,19 +103,30 @@ public class Calculadora extends AppCompatActivity {
         textoActual = "";
         textoTV.setText("");
     }
-
     public void mostrarEnPantalla(String valor) {
-
         boolean valorEsSimbolo = valor.equals("+") || valor.equals("-") || valor.equals("÷") || valor.equals("×") || valor.equals("%") || valor.equals(".");
         boolean valorEsIgual = valor.equals("=");
         boolean terminaConSimbolo = textoActual.endsWith("+") || textoActual.endsWith("-") || textoActual.endsWith("÷") || textoActual.endsWith("×") || textoActual.endsWith("%") || textoActual.equals(".");
-
         if (valorEsIgual){
-            String expr = textoActual;
+            String expr = textoActual.replace("%", "/100*").replace("×", "*").replace("÷", "/");
             Expression expression = new Expression(expr);
             double resultado = expression.calculate();
-            textoActual = textoTV.getText().toString() + valor;
+            textoActual = String.valueOf(resultado);
             textoTV.setText(textoActual);
+            if (textoTV.getText().toString() == "NaN"){
+                patricioIV.setVisibility(patricioIV.VISIBLE);
+                Glide.with(this).asGif().load(R.drawable.patricio).into(patricioIV);
+            } else {
+                patricioIV.setVisibility(patricioIV.GONE);
+                Glide.with(this).clear(patricioIV);
+            }
+            return;
         }
+        if (valorEsSimbolo && terminaConSimbolo) {
+            Toast.makeText(this, "Pendejo men", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        textoActual += valor;
+        textoTV.setText(textoActual);
     }
 }
